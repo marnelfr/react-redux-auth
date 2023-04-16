9a1d4063e71dd5f9d2b5779795c10330a4b4a340
Marnel Gnacadja <gmlginolias@gmail.com>
2023-04-16 11:17:29 +0100

    Added router to the main

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
