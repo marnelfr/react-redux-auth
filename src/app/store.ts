0354e736c7dea62db2590805de10dad635600d05
Marnel Gnacadja <gmlginolias@gmail.com>
2023-04-16 11:14:45 +0100

    Updated the store config

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/slices/authSlice";
+import { apiSlice } from "./api/apiSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
 
 export type AppDispatch = typeof store.dispatch;
